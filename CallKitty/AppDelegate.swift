40b999c021632aa7cfb87609f28450af4c219121
beepscore <support@beepscore.com>
2017-11-07 09:43:35 -0800

    Unstable WIP Add groups, copy files from reference project Speakerbox. TODO: Add more classes, replace storyboard and view controller, add IntentsExtension

--- CallKitty/AppDelegate.swift
+++ CallKitty/AppDelegate.swift
@@ -6,41 +6,86 @@
 //  Copyright © 2017 Beepscore LLC. All rights reserved.
 //
 
+/*
+ Abstract:
+ The application delegate.
+ */
+
 import UIKit
+import PushKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
+
+    class var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
     var window: UIWindow?
+    let pushRegistry = PKPushRegistry(queue: DispatchQueue.main)
+    let callManager = SpeakerboxCallManager()
+    var providerDelegate: ProviderDelegate?
 
+    // MARK: - UIApplicationDelegate
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        pushRegistry.delegate = self
+        pushRegistry.desiredPushTypes = [.voIP]
+
+        providerDelegate = ProviderDelegate(callManager: callManager)
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        guard let handle = url.startCallHandle else {
+            print("Could not determine start call handle from URL: \(url)")
+            return false
+        }
+
+        callManager.startCall(handle: handle)
+        return true
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        guard let handle = userActivity.startCallHandle else {
+            print("Could not determine start call handle from user activity: \(userActivity)")
+            return false
+        }
+
+        guard let video = userActivity.video else {
+            print("Could not determine video from user activity: \(userActivity)")
+            return false
+        }
+
+        callManager.startCall(handle: handle, video: video)
+        return true
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    // MARK: - PKPushRegistryDelegate
+
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        /*
+         Store push credentials on server for the active user.
+         For sample app purposes, do nothing since everything is being done locally.
+         */
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
+        guard type == .voIP else { return }
+
+        if let uuidString = payload.dictionaryPayload["UUID"] as? String,
+            let handle = payload.dictionaryPayload["handle"] as? String,
+            let hasVideo = payload.dictionaryPayload["hasVideo"] as? Bool,
+            let uuid = UUID(uuidString: uuidString)
+        {
+            displayIncomingCall(uuid: uuid, handle: handle, hasVideo: hasVideo)
+        }
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    /// Display the incoming call to the user
+    func displayIncomingCall(uuid: UUID, handle: String, hasVideo: Bool = false, completion: ((Error?) -> Void)? = nil) {
+        providerDelegate?.reportIncomingCall(uuid: uuid, handle: handle, hasVideo: hasVideo, completion: completion)
     }
-
 
 }
-
