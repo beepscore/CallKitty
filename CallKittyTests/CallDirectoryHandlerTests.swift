5d97dd289a73d1e91f2d60cb566a8ea33bbe33c7
beepscore <support@beepscore.com>
2017-11-07 19:02:08 -0800

    Add CallDirectoryHandlerTests, but comment it out.

---
+++ CallKittyTests/CallDirectoryHandlerTests.swift
@@ -0,0 +1,34 @@
+//
+//  CallDirectoryHandlerTests.swift
+//  CallKittyTests
+//
+//  Created by Steve Baker on 11/7/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+// TODO: consider try to get this working
+// currently unit tests won't run on device, not sure why.
+// this unit test won't run on simulator, I think CallKit requires device
+// so comment this test out for now.
+
+//import XCTest
+//import CallKit
+//@testable import CallKitty
+//
+//class CallDirectoryHandlerTests: XCTestCase {
+//
+//    func testBeginRequest() {
+//
+//        // normally iOS system provides the context when it calls beginRequest
+//        // for unit test, just instantiate a context
+//        let context = CXCallDirectoryExtensionContext()
+//
+//        // in simulator, build fails here
+//        // let handler = CallDirectoryHandler()
+//
+//        //handler.beginRequest(with: context)
+//        //XCTAssertNotNil(context.delegate)
+//    }
+//
+//}
+
