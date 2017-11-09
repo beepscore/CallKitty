312ed0a9b9318a1f31e8ee65b5c9e3cb71627147
beepscore <support@beepscore.com>
2017-11-08 20:39:34 -0800

    In PhoneNumberFormatterTests add tests

--- CallKittyTests/PhoneNumberFormatterTests.swift
+++ CallKittyTests/PhoneNumberFormatterTests.swift
@@ -11,8 +11,37 @@ import XCTest
 
 class PhoneNumberFormatterTests: XCTestCase {
 
+    // MARK: - test phoneNumber
+
     func testPhoneNumberNil() {
         XCTAssertNil(PhoneNumberFormatter.phoneNumber(phoneNumberString: nil))
     }
+
+    func testPhoneNumberEmptyString() {
+        XCTAssertNil(PhoneNumberFormatter.phoneNumber(phoneNumberString: ""))
+    }
+
+    func testPhoneNumberNoDigits() {
+        XCTAssertNil(PhoneNumberFormatter.phoneNumber(phoneNumberString: "abcsdaffEEEBIAFBM DSF"))
+    }
+
+    func testPhoneNumberAllDigits() {
+        let actual = PhoneNumberFormatter.phoneNumber(phoneNumberString: "12345678901234567890")
+        XCTAssertEqual(actual, 12345678901234)
+    }
+
+    func testPhoneNumberMixedString() {
+        let actual = PhoneNumberFormatter.phoneNumber(phoneNumberString: "abc123hij98(7)+")
+        XCTAssertEqual(actual, 123987)
+    }
+
+    // MARK: - test sanitizedPhoneNumberString
+
+    func testSanitizedPhoneNumberString() {
+        let actual = PhoneNumberFormatter.sanitizedPhoneNumberString(phoneNumberString: "12345678901234567890")
+        XCTAssertEqual(actual!.count, 14)
+        XCTAssertEqual(actual, "12345678901234")
+    }
+
 
 }
