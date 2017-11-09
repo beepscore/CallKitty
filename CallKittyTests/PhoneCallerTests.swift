64188809c6079cc1e9a18e852649611a0382d44d
beepscore <support@beepscore.com>
2017-11-09 12:45:15 -0800

    In PhoneCaller change phoneNumberString: String to phoneNumber: CXCallDirectoryPhoneNumber. Now phoneNumber is already correct type for use in contact directory for id or blocking, avoids potential fail when converting from String.
    Add placeholder values phoneNumberPlaceholder and labelPlaceholder.

--- CallKittyTests/PhoneCallerTests.swift
+++ CallKittyTests/PhoneCallerTests.swift
@@ -7,30 +7,63 @@
 //
 
 import XCTest
+// import CallKit for CXCallDirectoryPhoneNumber
+import CallKit
 @testable import CallKitty
 
 class PhoneCallerTests: XCTestCase {
-    
+
+    func testPhoneNumberPlaceholder() {
+        XCTAssertEqual(-1, PhoneCaller.phoneNumberPlaceholder)
+    }
+
+    func testLabelPlaceholder() {
+        XCTAssertEqual("", PhoneCaller.labelPlaceholder)
+    }
+
     func testConvenienceInit() {
 
-        let phoneNumberString = "1234567890"
+        let phoneNumber: CXCallDirectoryPhoneNumber = 1234567890
         let label = "foo"
 
-        let phoneCaller = PhoneCaller(phoneNumberString: phoneNumberString, label: label)
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber, label: label)
 
-        XCTAssertEqual(phoneCaller.phoneNumberString, phoneNumberString)
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, label)
     }
 
-    func testConvenienceInitLabelNil() {
+    func testConvenienceInitLabelDefault() {
 
-        let phoneNumberString = "abc123+;()?hij494"
-        let label: String? = nil
+        // Swift numeric literal can contain underscores to increase readability
+        let phoneNumber: CXCallDirectoryPhoneNumber = 802_745_6859
 
-        let phoneCaller = PhoneCaller(phoneNumberString: phoneNumberString, label: label)
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber)
 
-        XCTAssertEqual(phoneCaller.phoneNumberString, phoneNumberString)
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
+        XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
+    }
+
+    func testConvenienceInitLabelEmptyString() {
+
+        // Swift numeric literal can contain underscores to increase readability
+        let phoneNumber: CXCallDirectoryPhoneNumber = 01_802_745_6859
+        let label = ""
+
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber, label: label)
+
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, label)
+    }
+
+    func testConvenienceInitPhoneNumberMaximumValueWith15DigitsDoesntRollOver() {
+
+        // Swift numeric literal can contain underscores to increase readability
+        let phoneNumber: CXCallDirectoryPhoneNumber = 99999_99999_99999
+
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber)
+
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
+        XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
     }
     
 }
