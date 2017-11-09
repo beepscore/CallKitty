b270e50b21ab87552b219839fc456fee65417d2d
beepscore <support@beepscore.com>
2017-11-09 13:13:19 -0800

    In PhoneCaller revise documentation comments

--- CallKitty/PhoneCaller.swift
+++ CallKitty/PhoneCaller.swift
@@ -19,21 +19,21 @@ class PhoneCaller: Object {
     static let labelPlaceholder = ""
 
     /// objects must be available at runtime, so generally set default value
+    /// CXCallDirectoryPhoneNumber is an alias for Int64.
+    /// 2^64 ~ 10^19, so this type can hold ~ 19 decimal digits without overflow.
+    /// Most world phone numbers are <= 15 digits
+    /// http://www.itu.int/ITU-T/recommendations/rec.aspx?rec=E.164
+    /// https://stackoverflow.com/questions/723587/whats-the-longest-possible-worldwide-phone-number-i-should-consider-in-sql-varc#4729239
     @objc dynamic var phoneNumber: CXCallDirectoryPhoneNumber = phoneNumberPlaceholder
 
-    /// optional String, used for caller identification entry, not required for blocking entry
+    /// used for caller identification entry, not required for blocking entry
     @objc dynamic var label: String = labelPlaceholder
 
     /// convenience initializer
     ///
     /// - Parameters:
     ///   - phoneNumber: a phone number, can be used to identify and/or block phone calls
-    ///     CXCallDirectoryPhoneNumber is an alias for Int64.
-    ///     2^64 ~ 10^19, so this type can hold ~ 19 decimal digits without overflow.
-    ///     Most world phone numbers are <= 15 digits
-    ///     http://www.itu.int/ITU-T/recommendations/rec.aspx?rec=E.164
-    ///     https://stackoverflow.com/questions/723587/whats-the-longest-possible-worldwide-phone-number-i-should-consider-in-sql-varc#4729239
-    ///   - label: used to identify a caller. default value labelPlaceholder, e.g. empty string ""
+    ///   - label: used to identify a caller. default value labelPlaceholder, e.g. empty string
     convenience init(phoneNumber: CXCallDirectoryPhoneNumber, label: String = labelPlaceholder) {
         // must call designated initializer
         self.init()
