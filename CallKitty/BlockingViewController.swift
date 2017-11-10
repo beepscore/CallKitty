7cf7a8b8807ddb6207f3b362c7ee675c5c4dc5f7
beepscore <support@beepscore.com>
2017-11-09 20:33:42 -0800

    Add BlockingViewController

---
+++ CallKitty/BlockingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlockingViewController.swift
+//  CallKitty
+//
+//  Created by Steve Baker on 11/9/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class BlockingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
