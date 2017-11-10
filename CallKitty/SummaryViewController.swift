613570a8a43cd044055e89d7a95defcf525e3e97
beepscore <support@beepscore.com>
2017-11-09 18:49:27 -0800

    Add SummaryViewController, Summary.storyboard. In Main.storyboard connect tab bar controller to a storyboard reference to Summary.storyboard

---
+++ CallKitty/SummaryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SummaryViewController.swift
+//  CallKitty
+//
+//  Created by Steve Baker on 11/9/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = NSLocalizedString("SUMMARY_VC_TITLE", comment: "SummaryViewController title")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
