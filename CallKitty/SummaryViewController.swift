318795c44e3db5c87a236a806b0321054fc714ce
beepscore <support@beepscore.com>
2017-11-10 12:25:42 -0800

    In several view controllers delete commented out code from Xcode template

--- CallKitty/SummaryViewController.swift
+++ CallKitty/SummaryViewController.swift
@@ -74,14 +74,4 @@ class SummaryViewController: UIViewController {
         identifyingCountLabel.text = String(RealmService.shared.identifiedCount())
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
