7aa88193d416c6341936ee1a13656dd046234066
beepscore <support@beepscore.com>
2017-11-09 21:33:46 -0800

    Add IdViewController

---
+++ CallKitty/IdViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IdViewController.swift
+//  CallKitty
+//
+//  Created by Steve Baker on 11/9/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class IdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = NSLocalizedString("ID_VC_TITLE", comment: "IdViewController title")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
