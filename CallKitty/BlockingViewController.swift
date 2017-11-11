0b9cbdde9264dcb969a7afedc55e8718f5b0d997
beepscore <support@beepscore.com>
2017-11-11 14:59:29 -0800

    In searchBarSearchButtonClicked use phoneCaller to set some elements

--- CallKitty/BlockingViewController.swift
+++ CallKitty/BlockingViewController.swift
@@ -12,13 +12,16 @@ import CallKit
 class BlockingViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var phoneCallerPhoneNumberLabel: UILabel!
+    @IBOutlet weak var phoneCallerLabelTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         // when view loads, tab bar item displays this variable "title"
         title = NSLocalizedString("BLOCKING_VC_TITLE", comment: "BlockingViewController title")
+
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,10 +43,13 @@ extension BlockingViewController: UISearchBarDelegate {
 
         let phoneCaller = RealmService.getPhoneCaller(phoneNumber: phoneNumber, realm: RealmService.shared.realm)
 
-        if phoneCaller == nil {
-            print("not found")
-        } else {
+        if let phoneCaller = phoneCaller {
             // show phone number text fields for editing
+            phoneCallerPhoneNumberLabel.text = String(describing: phoneCaller.phoneNumber)
+            phoneCallerLabelTextField.text = phoneCaller.label
+        } else {
+            phoneCallerPhoneNumberLabel.text = "not found"
+            phoneCallerLabelTextField.text = ""
         }
     }
 
