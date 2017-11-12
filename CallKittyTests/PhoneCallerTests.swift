700c63e69c1624a35b9738969f0d4daeaa2aeee0
beepscore <support@beepscore.com>
2017-11-12 15:21:27 -0800

    In PhoneCaller tests revise and rename tests.

--- CallKittyTests/PhoneCallerTests.swift
+++ CallKittyTests/PhoneCallerTests.swift
@@ -21,7 +21,60 @@ class PhoneCallerTests: XCTestCase {
         XCTAssertEqual("", PhoneCaller.labelPlaceholder)
     }
 
-    func testConvenienceInit() {
+    func testConvenienceInitSupplyAllParameters() {
+
+        let phoneNumber: CXCallDirectoryPhoneNumber = 1234567890
+        let label = "foo"
+        let shouldBlock = true
+        let shouldIdentify = true
+        let shouldDelete = false
+
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber,
+            label: label,
+            hasChanges: true,
+            shouldBlock: shouldBlock,
+            isBlocked: false,
+            shouldIdentify: shouldIdentify,
+            isIdentified: false,
+            shouldDelete: shouldDelete)
+
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
+        XCTAssertEqual(phoneCaller.label, label)
+
+        XCTAssertTrue(phoneCaller.hasChanges)
+
+        XCTAssertEqual(phoneCaller.shouldBlock, shouldBlock)
+        XCTAssertFalse(phoneCaller.isBlocked)
+
+        // Note non-empty label doesn't automatically set shouldIdentify
+        XCTAssertEqual(phoneCaller.shouldIdentify, shouldIdentify)
+        XCTAssertFalse(phoneCaller.isIdentified)
+
+        XCTAssertEqual(phoneCaller.shouldDelete, shouldDelete)
+    }
+
+    func testConvenienceInitSupplyPhoneNumber() {
+
+        // Swift numeric literal can contain underscores to increase readability
+        let phoneNumber: CXCallDirectoryPhoneNumber = 802_745_6859
+
+        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber)
+
+        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
+        XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
+
+        XCTAssertTrue(phoneCaller.hasChanges)
+
+        XCTAssertFalse(phoneCaller.shouldBlock)
+        XCTAssertFalse(phoneCaller.isBlocked)
+
+        XCTAssertFalse(phoneCaller.shouldIdentify)
+        XCTAssertFalse(phoneCaller.isIdentified)
+
+        XCTAssertFalse(phoneCaller.shouldDelete)
+    }
+
+    func testConvenienceInitSupplyPhoneNumberLabelShouldBlock() {
 
         let phoneNumber: CXCallDirectoryPhoneNumber = 1234567890
         let label = "foo"
@@ -44,7 +97,7 @@ class PhoneCallerTests: XCTestCase {
         XCTAssertFalse(phoneCaller.shouldDelete)
     }
 
-    func testConvenienceInitLabelDefault() {
+    func testConvenienceInitSupplyPhoneNumberShouldBlock() {
 
         // Swift numeric literal can contain underscores to increase readability
         let phoneNumber: CXCallDirectoryPhoneNumber = 802_745_6859
@@ -54,10 +107,10 @@ class PhoneCallerTests: XCTestCase {
 
         XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
-        XCTAssertEqual(phoneCaller.shouldBlock, true)
+        XCTAssertTrue(phoneCaller.shouldBlock)
     }
 
-    func testConvenienceInitShouldBlockDefault() {
+    func testConvenienceInitSupplyPhoneNumberLabel() {
 
         // Swift numeric literal can contain underscores to increase readability
         let phoneNumber: CXCallDirectoryPhoneNumber = 802_745_6859
@@ -67,10 +120,10 @@ class PhoneCallerTests: XCTestCase {
 
         XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, "foo")
-        XCTAssertEqual(phoneCaller.shouldBlock, false)
+        XCTAssertFalse(phoneCaller.shouldBlock)
     }
 
-    func testConvenienceInitLabelEmptyStringShouldBlockDefault() {
+    func testConvenienceInitSupplyPhoneNumberLabelEmptyString() {
 
         // Swift numeric literal can contain underscores to increase readability
         let phoneNumber: CXCallDirectoryPhoneNumber = 01_802_745_6859
@@ -80,22 +133,8 @@ class PhoneCallerTests: XCTestCase {
 
         XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, label)
-        XCTAssertEqual(phoneCaller.shouldBlock, false)
+        XCTAssertFalse(phoneCaller.shouldBlock)
     }
-
-    func testConvenienceInitLabelDefaultShouldBlockDefault() {
-
-        // Swift numeric literal can contain underscores to increase readability
-        let phoneNumber: CXCallDirectoryPhoneNumber = 802_745_6859
-
-        // omit label: and shouldBlock:
-        let phoneCaller = PhoneCaller(phoneNumber: phoneNumber)
-
-        XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
-        XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
-        XCTAssertEqual(phoneCaller.shouldBlock, false)
-    }
-
 
     func testConvenienceInitPhoneNumberMaximumValueWith15DigitsDoesntRollOver() {
 
@@ -106,7 +145,7 @@ class PhoneCallerTests: XCTestCase {
 
         XCTAssertEqual(phoneCaller.phoneNumber, phoneNumber)
         XCTAssertEqual(phoneCaller.label, PhoneCaller.labelPlaceholder)
-        XCTAssertEqual(phoneCaller.shouldBlock, true)
+        XCTAssertTrue(phoneCaller.shouldBlock)
     }
     
 }
