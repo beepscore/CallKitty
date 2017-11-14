02885bdebb767306f890fbb71d9aebf2e53bc99e
beepscore <support@beepscore.com>
2017-11-14 09:40:51 -0800

    Add tests for statusColor

--- CallKittyTests/LongTableViewControllerTests.swift
+++ CallKittyTests/LongTableViewControllerTests.swift
@@ -12,7 +12,6 @@ import XCTest
 class LongTableViewControllerTests: XCTestCase {
 
     func testStatusStringAllTrue() {
-
         let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
             label: "foo",
             hasChanges: true,
@@ -26,7 +25,6 @@ class LongTableViewControllerTests: XCTestCase {
     }
 
     func testStatusString() {
-
         let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
                                       label: "foo",
                                       hasChanges: true,
@@ -39,5 +37,68 @@ class LongTableViewControllerTests: XCTestCase {
         XCTAssertEqual(LongTableViewController.statusString(phoneCaller: phoneCaller), "hc1sb1ib0si1ii0sd0")
     }
 
+    func testStatusColorAllTrue() {
+        let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
+                                      label: "foo",
+                                      hasChanges: true,
+                                      shouldBlock: true,
+                                      isBlocked: true,
+                                      shouldIdentify: true,
+                                      isIdentified: true,
+                                      shouldDelete: true)
+
+        XCTAssertEqual(LongTableViewController.statusColor(phoneCaller: phoneCaller), .lightGray)
+    }
     
+    func testStatusColorShouldBlock() {
+        let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
+                                      label: "foo",
+                                      hasChanges: true,
+                                      shouldBlock: true,
+                                      isBlocked: false,
+                                      shouldIdentify: false,
+                                      isIdentified: false,
+                                      shouldDelete: false)
+
+        XCTAssertEqual(LongTableViewController.statusColor(phoneCaller: phoneCaller), UIColor.callKittyPaleRed())
+    }
+
+    func testStatusColorShouldIdentify() {
+        let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
+                                      label: "foo",
+                                      hasChanges: true,
+                                      shouldBlock: false,
+                                      isBlocked: false,
+                                      shouldIdentify: true,
+                                      isIdentified: false,
+                                      shouldDelete: false)
+
+        XCTAssertEqual(LongTableViewController.statusColor(phoneCaller: phoneCaller), UIColor.callKittyPaleYellow())
+    }
+
+    func testStatusColorShouldBlockShouldIdentify() {
+        let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
+                                      label: "foo",
+                                      hasChanges: true,
+                                      shouldBlock: true,
+                                      isBlocked: false,
+                                      shouldIdentify: true,
+                                      isIdentified: false,
+                                      shouldDelete: false)
+
+        XCTAssertEqual(LongTableViewController.statusColor(phoneCaller: phoneCaller), .orange)
+    }
+
+    func testStatusColorAllFalse() {
+        let phoneCaller = PhoneCaller(phoneNumber: 1234567890,
+                                      label: "foo",
+                                      hasChanges: false,
+                                      shouldBlock: false,
+                                      isBlocked: false,
+                                      shouldIdentify: false,
+                                      isIdentified: false,
+                                      shouldDelete: false)
+
+        XCTAssertEqual(LongTableViewController.statusColor(phoneCaller: phoneCaller), .white)
+    }
 }
