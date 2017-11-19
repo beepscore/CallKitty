83a3d4b361d56f9ea472ec0846e6ed5fa73a5c60
beepscore <support@beepscore.com>
2017-11-18 19:09:48 -0800

    In AppDelegate comment out call to reloadExtension

--- CallKitty/AppDelegate.swift
+++ CallKitty/AppDelegate.swift
@@ -35,14 +35,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
 
         providerDelegate = ProviderDelegate(callManager: callManager)
 
+        // comment out this call. When running target CallKittyDirectoryExtension, it threw error at startup.
+        // reloadExtension The operation couldn’t be completed. (com.apple.CallKit.error.calldirectorymanager error 2.)
+        // CXErrorCodeCallDirectoryManagerErrorLoadingInterrupted = 2
         // https://stackoverflow.com/questions/43951781/callkit-extension-begin-request
-        CXCallDirectoryManager.sharedInstance.reloadExtension (
-            withIdentifier: "com.beepscore.CallKitty.CallKittyDirectoryExtension",
-            completionHandler: {(error) -> Void in
-                if let error = error {
-                    print("reloadExtension", error.localizedDescription)
-                }
-        })
+//        CXCallDirectoryManager.sharedInstance.reloadExtension (
+//            withIdentifier: "com.beepscore.CallKitty.CallKittyDirectoryExtension",
+//            completionHandler: {(error) -> Void in
+//                if let error = error {
+//                    print("reloadExtension", error.localizedDescription)
+//                }
+//        })
 
         return true
     }
