59ee720f146c1f286500346fbc705a139ecb160b
beepscore <support@beepscore.com>
2017-11-20 16:07:31 -0800

    In AppDelegate call getEnabledStatusForExtension and reloadExtension

--- CallKitty/AppDelegate.swift
+++ CallKitty/AppDelegate.swift
@@ -32,6 +32,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         providerDelegate = ProviderDelegate(callManager: callManager)
 
+        CXCallDirectoryManager.sharedInstance
+            .getEnabledStatusForExtension(withIdentifier: "com.beepscore.CallKitty.CallKittyDirectoryExtension",
+                                          completionHandler: {(enabledStatus, error)  -> Void in
+                                            if let error = error {
+                                                print("getEnabledStatusForExtension", error.localizedDescription)
+                                            }
+                                            var statusString = ""
+                                            switch enabledStatus {
+                                            case .unknown:
+                                                statusString = "unknown"
+                                            case .disabled:
+                                                statusString = "disabled"
+                                            case .enabled:
+                                                statusString = "enabled"
+                                            }
+                                            print("getEnabledStatusForExtension", statusString)
+            })
+
+        CXCallDirectoryManager.sharedInstance.reloadExtension (
+            withIdentifier: "com.beepscore.CallKitty.CallKittyDirectoryExtension",
+            completionHandler: {(error) -> Void in
+                if let error = error {
+                    print("reloadExtension", error.localizedDescription)
+                    // reloadExtension The operation couldn’t be completed. (com.apple.CallKit.error.calldirectorymanager error 6.)
+                    //CXErrorCodeCallDirectoryManagerErrorExtensionDisabled = 6
+                    // if get error 6, check Settings / Phone / Call blocking and identification / CallKitty switch on
+                }
+        })
+
         return true
     }
 
