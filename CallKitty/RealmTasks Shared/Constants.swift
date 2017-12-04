2e15d8095b34553ac8227602d78f38896e070a60
beepscore <support@beepscore.com>
2017-12-04 12:22:02 -0800

    Update Constants

--- CallKitty/RealmTasks Shared/Constants.swift
+++ CallKitty/RealmTasks Shared/Constants.swift
@@ -25,9 +25,10 @@ struct Constants {
     static let syncHost = localIPAddress
     #endif
 
-    static let syncRealmPath = "realmtasks"
-    static let defaultListName = "My Tasks"
-    static let defaultListID = "80EB1620-165B-4600-A1B1-D97032FDD9A0"
+    static let syncRealmPath = "realmcallers"
+    static let defaultListName = "My Callers"
+    // manually added 1 to defaultListID from sample project
+    static let defaultListID = "80EB1620-165B-4600-A1B1-D97032FDD9A1"
 
     static let syncServerURL = URL(string: "realm://\(syncHost):9080/~/\(syncRealmPath)")
     static let syncAuthURL = URL(string: "http://\(syncHost):9080")!
